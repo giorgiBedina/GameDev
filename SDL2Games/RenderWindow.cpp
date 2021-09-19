d10dd8b331c92d7b279d186fe5296684cb15f071
giorgi <Giorgi bedinashvili>
2021-09-19 15:54:09 +0400

    create window and renderer

---
+++ SDL2Games/RenderWindow.cpp
@@ -0,0 +1,13 @@
+#include "RenderWindow.hpp"
+
+RenderWindow::RenderWindow(const char* w_tittle, int w_width, int w_heigh, bool fullscreen)
+    :window(NULL), renderer(NULL)
+{
+    window = SDL_CreateWindow(w_tittle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w_width,  w_heigh,  fullscreen);
+    if(window == NULL)
+    {
+        std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
